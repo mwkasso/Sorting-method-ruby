ad866a92e59368a257b2139db8a553a29502939c
Mohammed Kasso <mkasso@spartaglobal.com>
2015-02-26 12:59:56 +0000

    Sorting will place all strings at the end and integers at the start of the array

--- sorting.rb
+++ sorting.rb
@@ -1,4 +1,4 @@
-numbers = [4,2,5,7,8,1,3, 'bsdkjv', 'asfd', 'asekf', 'csdjkfn']
+numbers = [4,2,5,7,8,1,3, 'bsdkjv',7, 'asfd', 'asekf', 6,'csdjkfn', 5]
 
 numbers.length.times do |x|
   numbers.length.times do |y|
@@ -15,6 +15,11 @@ numbers.length.times do |x|
         numbers[x] = numbers[y]
         numbers[y] = temp
       end
+    end 
+    if numbers[x].class <= Integer && numbers[y].class <= String
+        temp = numbers[x]
+        numbers[x] = numbers[y]
+        numbers[y] = temp
     end
   end
 end
